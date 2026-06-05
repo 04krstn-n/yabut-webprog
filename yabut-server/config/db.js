f9498f15aad887c7c01bb060849b23e365f2a41d
04krstn-n <kc7345497@gmail.com>
2026-06-06 05:13:06 +0800

    Fix Vercel server crash

--- yabut-server/config/db.js
+++ yabut-server/config/db.js
@@ -8,7 +8,6 @@ const connectDB = async () => {
     }
 
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Error: ${error.message}`);
